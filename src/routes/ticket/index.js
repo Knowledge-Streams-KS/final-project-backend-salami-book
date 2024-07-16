291a4a159a91aa541eeea2de65a66c82490fa58c
uswamunir40 <uswa40munir@gmail.com>
2024-07-16 23:29:56 +0500

    Sales generated

--- src/routes/ticket/index.js
+++ src/routes/ticket/index.js
@@ -2,7 +2,7 @@ import ticketController from "../../controller/Ticket/index.js";
 import { Router } from "express";
 
 const ticketRouter = Router();
-
+ticketRouter.get("/tickets", ticketController.getAll);
 ticketRouter.post('/ticket', ticketController.create)
 
 export default ticketRouter;
